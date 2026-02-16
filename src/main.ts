2d5574fc8c9c723a91bd50bbaddef6789999603a
AhmedUKamel <dev.ahmedkamel@gmail.com>
2026-02-16 21:25:18 +0200

    Add root endpoint
    - Add GET mapping into / to return response with server uptime seconds

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,21 @@ import {
 } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
 import { IServerConfig } from '@infra/config/server/server.types';
+import { RequestMethod, VersioningType } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   const configService = app.get(ConfigService);
 
